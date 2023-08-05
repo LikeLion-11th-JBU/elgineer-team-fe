2f7b2bf3d70020674fac86f42547d5f7362e2296
Leejinuk <leejin7900@gmail.com>
2023-08-05 19:36:19 +0900

    Feat: 초기세팅

---
+++ elgineer/src/App.js
@@ -0,0 +1,13 @@
+import "../src/index.css";
+
+function App() {
+  return (
+    <div>
+      <a className="p-2 font-bold text-red-500" href="#">
+        Hello world!
+      </a>
+    </div>
+  );
+}
+
+export default App;
